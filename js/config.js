5ba0c2275b14af6f151c69336331bbf6f45a8a7a
FirstKing1995 <dhionematheus@gmail.com>
2026-09-22 18:09:52 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 /* Cole aqui a URL do App da Web do Apps Script (termina em /exec).
    Enquanto estiver vazia, o jogo roda normal, só sem conta e ranking. */
 window.SUBSOLO_CONFIG = {
-  API_URL: ''
+  API_URL: 'https://script.google.com/macros/s/AKfycbxlCo_bFPIYSobX78uUMsFRdwO8XkzPAFrceyDgCuR7vUPsmYwGeJj9OvzsnS5rodUM/exec'
 };
